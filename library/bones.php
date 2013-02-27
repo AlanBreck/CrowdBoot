f4b0f1315386d5f18c26eac7397ceab82610f362
Jacob Lamont <jlamont@crowdboot.com>
2013-02-27 01:00:48 -0500

    Installed FitVids.js

--- library/bones.php
+++ library/bones.php
@@ -144,6 +144,7 @@ function bones_scripts_and_styles() {
     //adding scripts file in the footer
     wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/scripts.js', array( 'jquery' ), '', true );
     wp_register_script( 'formalize', get_stylesheet_directory_uri() . '/library/js/jquery.formalize.min.js', array( 'jquery' ), '', true );
+    wp_register_script( 'fitvids', get_stylesheet_directory_uri() . '/library/js/jquery.fitvids.js', array( 'jquery' ), '', true );
 
     // enqueue styles and scripts
     wp_enqueue_script( 'bones-modernizr' );
@@ -158,6 +159,7 @@ function bones_scripts_and_styles() {
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script( 'bones-js' );
     wp_enqueue_script( 'formalize' );
+    wp_enqueue_script( 'fitvids' );
 
   }
 }
