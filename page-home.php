1170c09aceadba6f3e4d2b7aec0117338bae833c
Jacob Lamont <jacob@libertyalliance.com>
2013-05-20 23:59:20 -0400

    Style Home Page and Change Fonts

--- page-home.php
+++ page-home.php
@@ -5,22 +5,32 @@ Template Name: Home Page
 ?>
 <?php get_header(); ?>
 
+			<div class="home-menu-container">
+				<ul class="home-menu clearfix wrap">
+					<li><a href="<?php echo site_url(); ?>/projects">Browse Projects</a></li>
+					<li><a href="<?php echo site_url(); ?>/create-a-project/">Create a Project</a></li>
+				</ul>
+			</div>
+
 			<div id="content">
 
 				<div id="inner-content" class="wrap clearfix">
 
-				    <div id="main" class="eightcol first clearfix" role="main">
+				    <div id="main" class="clearfix" role="main">
 
-						<article class="box1">
-							<p>What is Crowdfunding?</p>
+						<article class="first fourcol box1">
+							<h2>What is Crowdfunding?</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, omnis, voluptates, ab totam sit deleniti molestias soluta maxime facilis sunt minus esse inventore quaerat in minima nostrum vel error officia.</p>
 						</article>
 
-						<article class="box2">
-							<p>Who is CrowdBoot?</p>
+						<article class="fourcol box2">
+							<h2>Who is CrowdBoot?</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, omnis, voluptates, ab totam sit deleniti molestias soluta maxime facilis sunt minus esse inventore quaerat in minima nostrum vel error officia.</p>
 						</article>
 
-						<article class="box3">
-							<p>What customers does CrowdBoot serve?</p>
+						<article class="last fourcol box3">
+							<h2>What customers do CrowdBoot serve?</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, omnis, voluptates, ab totam sit deleniti molestias soluta maxime facilis sunt minus esse inventore quaerat in minima nostrum vel error officia.</p>
 						</article>
 
 				    </div> <!-- end #main -->
